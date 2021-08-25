6160d53962c194043645a75197288d05e3a8dda5
admin <canadn02@gmail.com>
2021-08-25 19:01:19 +0200

    son guncelleme yapildi

--- src/test/java/linnProject/runners/Runner.java
+++ src/test/java/linnProject/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue="linnProject/stepdefinitions",
 
-        tags="@TC_1205", // work in progress
+        tags="@US_007", // work in progress
 
         dryRun=false
 
