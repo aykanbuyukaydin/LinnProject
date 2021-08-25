7505d9c8a5bd1997f0557cb0e75d9bb5020dfe09
unknown <email_adresiniz>
2021-08-25 22:41:51 +0300

    guncelleme suheylauz

--- src/test/java/linnProject/runners/Runner.java
+++ src/test/java/linnProject/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue="linnProject/stepdefinitions",
 
-        tags="@TC_1205", // work in progress
+        tags="@tc1001", // work in progress
 
         dryRun=false
 
