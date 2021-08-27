6b84da8beddddd52efcdabbb931378a9f8c8ed08
KaanKizilbaba <kizilbabaa@gmail.com>
2021-08-27 19:24:47 +0200

    us1

--- src/test/java/linnProject/stepdefinitions/US015StepDefinitions.java
+++ src/test/java/linnProject/stepdefinitions/US015StepDefinitions.java
@@ -9,10 +9,10 @@ import linnProject.utilities.Driver;
 public class US015StepDefinitions {
     US015Page us015Page = new US015Page();
 
-    @Given("kullanici {string} sayfasina gider")
-    public void kullaniciSayfasinaGider(String url_home) {
-        Driver.getDriver().get(ConfigReader.getProperty("url_home"));
-    }
+//    @Given("kullanici {string} sayfasina gider")
+//    public void kullaniciSayfasinaGider(String url_home) {
+//        Driver.getDriver().get(ConfigReader.getProperty("url_home"));
+//    }
 
     @Given("insan simgesine tiklar")
     public void insan_simgesine_tiklar() {
