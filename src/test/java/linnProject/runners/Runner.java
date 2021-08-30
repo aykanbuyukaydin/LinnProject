a3b0e8f2c47a061013d5d192a28735828ae5403d
Unaldi <munaldi026@gmail.com>
2021-08-30 21:11:34 +0300

    munaldi

--- src/test/java/linnProject/runners/Runner.java
+++ src/test/java/linnProject/runners/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue="linnProject/stepdefinitions",
 
-        tags="@TC_1205", // work in progress
+        tags="@TC_1202", // work in progress
 
         dryRun=false
 
