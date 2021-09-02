b28691822434fbf69da0c46d8850fe314e93f355
esadeymen <esadeymen@gmail.com>
2021-09-02 22:26:11 +0300

    database calisma

--- src/test/java/linnProject/stepdefinitions/US022Api_StepDefinitions.java
+++ src/test/java/linnProject/stepdefinitions/US022Api_StepDefinitions.java
@@ -1,10 +1,15 @@
 package linnProject.stepdefinitions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import linnProject.pojos.Country_Ders_linn;
+import linnProject.pojos.States_Api_US22;
 import linnProject.utilities.ConfigReader;
+
+import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
 
@@ -30,7 +35,9 @@ public class US022Api_StepDefinitions {
     }
 
     @Then("Verilen eyaletlerin doğruluğunu kontrol eder ab")
-    public void verilen_eyaletlerin_doğruluğunu_kontrol_eder_ab() {
+    public void verilen_eyaletlerin_doğruluğunu_kontrol_eder_ab() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        States_Api_US22[] states_api_us22 = objectMapper.readValue(response.asString(), States_Api_US22[].class);
 
     }
 }
